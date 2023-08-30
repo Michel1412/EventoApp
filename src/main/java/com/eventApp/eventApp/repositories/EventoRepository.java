9456e71839a396af4780a5a42d46cb3b678f6cf2
Michel1412 <michelbocchijr@gmail.com>
2023-08-30 11:50:27 -0300

    Conectado com o insomnia, NAO terminado (parado na parte de cadastro de convidados)

--- src/main/java/com/eventApp/eventApp/repositories/EventoRepository.java
+++ src/main/java/com/eventApp/eventApp/repositories/EventoRepository.java
@@ -2,7 +2,10 @@ package com.eventApp.eventApp.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import com.eventApp.eventApp.models.Evento;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public interface EventoRepository extends CrudRepository<Evento, String > {
 
+    Evento findByCodigo(long codigo);
 
 }
